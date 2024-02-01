8a6836c2a6ba9469bf8178d08085b1e21b8ff91b
Umangjais01 <jaiswalumang01@gmail.com>
2024-02-01 17:55:49 +0530

    crated pages +login +browse +body +header
    please add image links for background in body and logo in header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="absolute"> 
+       
+        <img className='w-40'
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUxraElSdw-Yd2jxyw-SPl4OkaBq389yf5_Q&usqp=CAU"
+            alt="logo"/>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "./Header"
+const Login = () => {
+  return (
+    <div>
+        <Header />
+        <div>
+            <img src="https://cdn.mos.cms.futurecdn.net/rDJegQJaCyGaYysj2g5XWY-320-80.jpg"
+            alt="logo"/>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Login
